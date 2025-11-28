606d4204c44009e86f4bb0f1ebfb8bc6c6dc5634
Esli Rodrigues <eslicdm1@gmail.com>
2025-11-28 15:34:01 -0300

    Initial commit

---
+++ app-spring-ai/src/main/java/com/eslirodrigues/app_spring_ai/AppSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.eslirodrigues.app_spring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringAiApplication.class, args);
+	}
+
+}
---
+++ app-spring-ai/src/test/java/com/eslirodrigues/app_spring_ai/AppSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eslirodrigues.app_spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
